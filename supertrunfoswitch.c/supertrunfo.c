c4ca8a57ebe0f7e3a62dbc376bb4fbda0f3ab587
CesarGaldino <cesargaldino1993@gmail.com>
2025-03-30 22:29:13 +0000

    finalizei exercicio

--- supertrunfoswitch.c/supertrunfo.c
+++ supertrunfoswitch.c/supertrunfo.c
@@ -32,7 +32,7 @@ printf("População: %d bilhões\n",populacao2);
 printf(" Area: %.2f km²\n", area2);
 printf(" PIB: %.2f \n",pib2);
 printf(" Pontos Turisticos: %d \n",pontos2);
-printf("Densidade: %.2f \n",&densidade2),
+printf("Densidade: %.2f \n",&densidade2);
 
 printf("### Escolha uma das Opções : ###\n");
 printf("1. População\n");
@@ -45,8 +45,10 @@ scanf("%d",&opcoes);
 switch (opcoes)
 {
 case 1:
-     printf("População Carta 1 x População Carta 2\n");
-     
+     printf("*** População Carta 1 x População Carta 2 ***\n");
+     printf("População Carta 1: %d bilhões\n",populacao1);
+     printf("População Carta 2: %d bilhões\n",populacao2);
+
      if (populacao1 > populacao2){
         printf("### Carta 1 Venceu!! ###");
     } else if (populacao1 == populacao2){
@@ -59,8 +61,11 @@ case 1:
 
 
 case 2:
-     printf("Area Carta 1 x Area Carta 2\n");
-     if (area1 > area2){
+     printf("*** Area Carta 1 x Area Carta 2 ***\n");
+     printf(" Area Carta 1: %.2f km²\n", area1);
+     printf(" Area Carta 2: %.2f km²\n", area2);
+
+    if (area1 > area2){
         printf("### Carta 1 Venceu!! ###");
     } else if (area1 == area2){
         printf("### Houve Empate!! ###");
@@ -70,7 +75,10 @@ case 2:
     }
      break;
 case 3:
-     printf("Pib Carta 1 x Pib Carta 2\n");
+     printf("*** Pib Carta 1 x Pib Carta 2 ***\n");
+     printf(" PIB Carta 1: %.2f \n",pib1);
+     printf(" PIB Carta 2: %.2f \n",pib2);
+
      if (pib1 > pib2){
         printf("### Carta 1 Venceu!! ###");
     } else if (pib1 == pib2){
@@ -81,7 +89,10 @@ case 3:
     }
      break;
 case 4:
-     printf("Pontos Turisticos Carta 1 x Pontos Turisticos Carta 2\n");
+     printf("*** Pontos Turisticos Carta 1 x Pontos Turisticos Carta 2 ***\n");
+     printf(" Pontos Turisticos Carta 1: %d \n",pontos1);
+     printf(" Pontos Turisticos Carta 2: %d \n",pontos2);
+
      if (pontos1 > pontos2){
         printf("### Carta 1 Venceu!! ###");
     } else if (pontos1 == pontos2){
@@ -91,9 +102,12 @@ case 4:
         printf("### Carta 2 Venceu ###");
     }
     case 5:
-     printf("Densidade Carta 1 x Densidade Carta 2\n");
+     printf("*** Densidade Carta 1 x Densidade Carta 2 ***\n");
+     printf("Densidade Carta 1: %.2f \n",&densidade1);
+     printf("Densidade Carta 2: %.2f \n",&densidade2);
+
      
-     if (densidade1 > densidade2){
+     if (densidade1 < densidade2){
         printf("### Carta 1 Venceu!! ###");
     } else if (densidade1 == densidade2){
         printf("### Houve Empate!! ###");
